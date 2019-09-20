1cea95531c9a9bb6286d64ea7921efa912ea5ba6
abel enrique <abel@abel.com>
2019-09-19 21:45:17 -0500

    contiene los archivos iniciales cabecera

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Blog de Videojuegos</title>
+    </head>
+    <body>
+        <!-- cabecera --> 
+        <header>
+            <div id="logo">
+                <a href="index.php">
+                    Blog de Videojuegos
+                </a>
+            </div>
+        </header>
+        
+        <!-- menu --> 
+        
+        <!-- barra lateral -->
+        
+        <!-- caja principal -->
+        
+        <!-- pie de pagina -->
+    </body>
+</html>
