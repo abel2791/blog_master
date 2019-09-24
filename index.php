9e1b7c4faff7f31d93d66ac182f357c095639a07
abel enrique <abel@abel.com>
2019-09-24 16:20:54 -0500

    Estilos del contenido principal

--- index.php
+++ index.php
@@ -85,35 +85,49 @@
         <div id="principal">
             <h1>Ultimas entradas</h1>
             <article class="entrada">
+                <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras urna nunc, suscipit sit amet rutrum at, posuere quis massa. Nulla facilisi. Proin volutpat elit quis mauris finibus, a eleifend quam posuere. Sed mollis gravida tincidunt. Praesent leo libero, feugiat quis pulvinar vitae, finibus vitae lectus. Ut libero felis, ultricies vitae ultrices at, tincidunt sed nisl. Duis luctus velit in vulputate faucibus. Sed interdum tincidunt magna, eu aliquet nulla facilisis nec. Maecenas fermentum pretium mauris, in congue risus venenatis eu. Aenean accumsan tellus at dui gravida malesuada. Sed in sodales dolor. Morbi dictum tellus in tincidunt posuere.
                 </p>
+                </a>
             </article>
             
             <article class="entrada">
+                <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras urna nunc, suscipit sit amet rutrum at, posuere quis massa. Nulla facilisi. Proin volutpat elit quis mauris finibus, a eleifend quam posuere. Sed mollis gravida tincidunt. Praesent leo libero, feugiat quis pulvinar vitae, finibus vitae lectus. Ut libero felis, ultricies vitae ultrices at, tincidunt sed nisl. Duis luctus velit in vulputate faucibus. Sed interdum tincidunt magna, eu aliquet nulla facilisis nec. Maecenas fermentum pretium mauris, in congue risus venenatis eu. Aenean accumsan tellus at dui gravida malesuada. Sed in sodales dolor. Morbi dictum tellus in tincidunt posuere.
                 </p>
+                </a>
             </article>
             
             <article class="entrada">
+                <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras urna nunc, suscipit sit amet rutrum at, posuere quis massa. Nulla facilisi. Proin volutpat elit quis mauris finibus, a eleifend quam posuere. Sed mollis gravida tincidunt. Praesent leo libero, feugiat quis pulvinar vitae, finibus vitae lectus. Ut libero felis, ultricies vitae ultrices at, tincidunt sed nisl. Duis luctus velit in vulputate faucibus. Sed interdum tincidunt magna, eu aliquet nulla facilisis nec. Maecenas fermentum pretium mauris, in congue risus venenatis eu. Aenean accumsan tellus at dui gravida malesuada. Sed in sodales dolor. Morbi dictum tellus in tincidunt posuere.
                 </p>
+                </a>
             </article>
             
             <article class="entrada">
+                <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras urna nunc, suscipit sit amet rutrum at, posuere quis massa. Nulla facilisi. Proin volutpat elit quis mauris finibus, a eleifend quam posuere. Sed mollis gravida tincidunt. Praesent leo libero, feugiat quis pulvinar vitae, finibus vitae lectus. Ut libero felis, ultricies vitae ultrices at, tincidunt sed nisl. Duis luctus velit in vulputate faucibus. Sed interdum tincidunt magna, eu aliquet nulla facilisis nec. Maecenas fermentum pretium mauris, in congue risus venenatis eu. Aenean accumsan tellus at dui gravida malesuada. Sed in sodales dolor. Morbi dictum tellus in tincidunt posuere.
                 </p>
-            </article>           
-        </div>
-        
+                </a>
+            </article>
+                    <div id="ver-todas">
+                       <a href="">Ver todas las entradas</a>
+                    </div>  
+        </div><!--fin principal-->
+    
+        <div class="clearfix"></div><!---para que el pie de pagina no suba,hacia arriba--->
       </div>
+        
+        
         <!-- pie de pagina -->
         <footer id="pie">
             <p>Desarrollado por Enrique Abel &copy; 2019</p>
