73637167cd0eae1868f702ffa6869da803b4693f
abel enrique <abel@abel.com>
2019-09-25 21:54:46 -0500

    se agrego includes

---
+++ includes/cabecera.php
@@ -0,0 +1,47 @@
+<?php require_once 'conexion.php';?>
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Blog de Videojuegos</title>
+        <link rel="stylesheet" type="text/css" href="./assets/css/style.css"/>
+    </head>
+    <body>
+        <!-- Logo --> 
+        <header id="cabecera">
+            <div id="logo">
+                <a href="index.php">
+                    Blog de Videojuegos
+                </a>
+            </div>            
+           <!-- menu --> 
+           <nav id="menu">
+               <ul>
+                   <li>
+                       <a href="index.php">Inicio</a>
+                   </li>
+                   <li>
+                       <a href="index.php">Categoria 1</a>
+                   </li>
+                   <li>
+                       <a href="index.php">Categoria 2</a>
+                   </li>
+                   <li>
+                       <a href="index.php">Categoria 3</a>
+                   </li>
+                   <li>
+                       <a href="index.php">Categoria 4</a>
+                   </li>
+                   <li>
+                       <a href="index.php">Sobre mi</a>
+                   </li>
+                   <li>
+                       <a href="index.php">Contacto</a>
+                   </li>
+                   
+               </ul>
+           </nav>
+           
+           <div class="clearfix"></div><!---para que borre todos los flotados para que lo de abajo
+           no se monte con lo de arriba-->
+        </header>
---
+++ includes/conexion.php
@@ -0,0 +1,9 @@
+<?php
+//Conexion
+$server = 'localhost';
+$username = 'root';
+$password = 'toor';
+$database = 'blog_master';
+$db = mysqli_connect($server, $username, $password, $database);
+
+mysqli_query($db, "SET NAMES 'utf8'");
