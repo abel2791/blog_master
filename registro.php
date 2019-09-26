ec256cd8369aa5203840c7d91b9560c40ad71267
abel enrique <abel@abel.com>
2019-09-25 23:25:43 -0500

    registro de usuarios

--- registro.php
+++ registro.php
@@ -1,4 +1,55 @@
 <?php
+if(isset($_POST)){
+    
+    //recoger los valores del formulario
+    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
+    $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
+    $email = isset($_POST['email']) ? $_POST['email'] : false;
+    $password = isset($_POST['password']) ? $_POST['password'] : false;
+    
+    //Array de errores
+    $errores = array();
+    
+    //validar los datos antes de guardarlos en la base de datos :)  
+    //validar el nombre
+    if(!empty($nombre) && !is_numeric($nombre) && !preg_match("/[0-9]/", $nombre)){
+        $nombre_validado = true;
+    }else{
+        $nombre_validado = false;
+        $errores['nombre'] = "El nombre no es valido";
+    }
+    
+    //validar apellidos
+    if(!empty($apellidos) && !is_numeric($apellidos) && !preg_match("/[0-9]/", $apellidos)){
+        $apellidos_validado = true;
+    }else{
+        $apellidos_validado = false;
+        $errores['apellidos'] = "Los apellidos no son validos";
+    }
+    
+    //validar email
+     if(!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $email_validado = true;
+    }else{
+        $email_validado = false;
+        $errores['email'] = "el email no es valido";
+    }
+    
+    //validar contraseña
+     if(!empty($password)){
+        $password_validado = true;
+    }else{
+        $password_validado = false;
+        $errores['password'] = "La contraseña esta vacia";
+    }
+    
+    
+    $guardar_usuario=false;
+    if(count($errores) == 0){
+        $guardar_usuario = true;
+        //insertar usuario en la base de datos en la tabla usuarios
+    }
+}
 
-var_dump($_POST);
+
 
