05f4be828340b8a0e45d019b84a778ba51ee1807
abel enrique <abel@abel.com>
2019-09-25 21:59:16 -0500

    se agrego la barra lateral a include

--- index.php
+++ index.php
@@ -1,40 +1,7 @@
 <?php require_once 'includes/cabecera.php';?>
-<div id="contenedor">        
-        <!-- barra lateral -->
-        <aside id="sidebar">
-            <div id="login" class="bloque">
-                <h3>Identificate</h3>
-                <form action="login.php" method="POST">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" />
-                    
-                     <label for="password">Contraseña</label>
-                     <input type="password" name="password" />
-                     
-                     <input type="submit" value="Entrar"/>
-                </form>                
-            </div>
-            
-            <div id="register" class="bloque">
-                <h3>Registrate</h3>
-                <form action="registro.php" method="POST">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" name="nombre" />
-                    
-                    <label for="apellidos">Apellidos</label>
-                    <input type="text" name="apellidos" />
-                    
-                    <label for="email">Email</label>
-                    <input type="email" name="email" />
-                    
-                     <label for="password">Contraseña</label>
-                     <input type="password" name="password" />
-                     
-                     <input type="submit" value="Registrar"/>
-                </form>                
-            </div>
-        </aside>
-        
+      
+      <div id="contenedor">        
+      <?php require_once 'includes/lateral.php';?>;  
         <!-- caja principal -->
         <div id="principal">
             <h1>Ultimas entradas</h1>
