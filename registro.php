d35e32a5775c13e7e9fd51bddab0c8f6d2b12bb1
abel enrique <abel@abel.com>
2019-09-28 15:51:47 -0500

    se agrego escapar datos a registro

--- registro.php
+++ registro.php
@@ -7,15 +7,16 @@ if(isset($_POST)){
     require_once 'includes/conexion.php';
 
    //iniciar session
-   session_start();
-    
+    if($_SESSION){
+    session_start();
+    }
     
     
     //recoger los valores del formulario
-    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
-    $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
-    $email = isset($_POST['email']) ? $_POST['email'] : false;
-    $password = isset($_POST['password']) ? $_POST['password'] : false;
+    $nombre = isset($_POST['nombre']) ? mysqli_real_escape_string($db, $_POST['nombre']) : false;
+    $apellidos = isset($_POST['apellidos']) ? mysqli_real_escape_string($db,$_POST['apellidos']) : false;
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($db, $_POST['email']) : false;
+    $password = isset($_POST['password']) ? mysqli_real_escape_string($db, $_POST['password']) : false;
     
     //Array de errores
     $errores = array();
