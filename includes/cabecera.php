e06b3cdc3fa81802ce9485827467c33845f8a820
abel enrique <abel@abel.com>
2019-09-30 18:29:49 -0500

    se agrego un if

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -24,12 +24,17 @@
                    </li>
                    <?php 
                         $categorias = conseguirCategorias($db);
+                        if(!empty($categorias)):
                         while ($categoria = mysqli_fetch_assoc($categorias)):
                    ?>
                       <li>
                          <a href="categoria.php?id=<?=$categoria[id]?>"><?=$categoria['nombre']?></a>
                       </li>
-                   <?php endwhile; ?>
+                   <?php 
+                        endwhile;
+                   endif;
+                   
+                   ?>
                    <li>
                        <a href="index.php">Sobre mi</a>
                    </li>
