92340d9d3ad030f1692ea8f6d79d6e03b7249d23
abel enrique <abel@abel.com>
2019-09-29 20:43:40 -0500

    se agrego la funcion conseguirCategorias en cabecera.php

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -14,25 +14,22 @@
                 <a href="index.php">
                     Blog de Videojuegos
                 </a>
-            </div>            
-           <!-- menu --> 
+            </div>       
+            
+           <!-- menu -->
            <nav id="menu">
                <ul>
                    <li>
                        <a href="index.php">Inicio</a>
                    </li>
-                   <li>
-                       <a href="index.php">Categoria 1</a>
-                   </li>
-                   <li>
-                       <a href="index.php">Categoria 2</a>
-                   </li>
-                   <li>
-                       <a href="index.php">Categoria 3</a>
-                   </li>
-                   <li>
-                       <a href="index.php">Categoria 4</a>
-                   </li>
+                   <?php 
+                        $categorias = conseguirCategorias();
+                        while ($categoria = mysqli_fetch_assoc($categorias)):
+                   ?>
+                      <li>
+                         <a href="categoria.php?id=<?=$categoria[id]?>"><?=$categorias['nombre']?></a>
+                      </li>
+                   <?php endwhile; ?>
                    <li>
                        <a href="index.php">Sobre mi</a>
                    </li>
