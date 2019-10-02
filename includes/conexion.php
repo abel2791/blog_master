81503f2bac8abb5b660968d129d02659d53b9e4a
abel enrique <abel@abel.com>
2019-10-01 21:08:39 -0500

    se agrego una condicion para la sesion

--- includes/conexion.php
+++ includes/conexion.php
@@ -9,4 +9,6 @@ $db = mysqli_connect($server, $username, $password, $database);
 mysqli_query($db, "SET NAMES 'utf8'");
 
 //iniciar la sesion
-session_start();
+if(!isset($_SESSION)){
+    session_start();
+}
--- includes/redireccion.php
+++ includes/redireccion.php
@@ -1,8 +1,12 @@
 <?php
+session_start();
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//antes de iniciar la sesion, comprobar si existe la sesion
+if(!isset($_SESSION)){
+    session_start();
+}
 
+//comprobar sino esta la sesion
+if(!isset($_SESSION['usuario'])){
+    header("Location: index.php");
+}
