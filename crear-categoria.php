94a2a42527dbfba075f6d68ea521f89157596f74
abel enrique <abel@abel.com>
2019-10-01 21:25:59 -0500

    se agrego input de nombre

--- crear-categoria.php
+++ crear-categoria.php
@@ -5,6 +5,17 @@
  <!-- caja principal -->
         <div id="principal">
             <h1>Crear categorias</h1>
+            <p>
+                Añade nuevas categorias al blog para que los usuarios puedan 
+                usarlas al crear sus entradas.
+            </p>
+            <br>
+            <form action="guardar-categoria.php" method="POST">
+                <label>Nombre de la categoria</label>
+                <input type="nombre" name="nombre"/>
+                
+                <input type="submit" value="Gaurdar"/>
+            </form>
             
                   
         </div><!--fin de caja principal-->
