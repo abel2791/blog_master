bc911d756c87fddf645d9dbf981a2aa70ebaad5f
abel enrique <abel@abel.com>
2020-01-05 20:30:25 -0600

    se agrego link a categoria href

--- entrada.php
+++ entrada.php
@@ -13,8 +13,11 @@
        <!-- caja principal -->
         <div id="principal">
             
+            
             <h1><?=$entrada_actual['titulo']?></h1>
+            <a href="categoria.php?id=<?=$entrada_actual['categoria_id']?>">            
             <h2><?=$entrada_actual['categoria']?></h2>
+            </a>
             <h4><?=$entrada_actual['fecha']?></h4>
             <p>
                 <?=$entrada_actual['descripcion']?>
