8a4ebd5ccf805a23cf14cc4d7846d7463230e485
abel enrique <abel@abel.com>
2020-01-09 22:29:13 -0600

    se corrige nombre de la tabla en la consulta delete

--- borrar-entrada.php
+++ borrar-entrada.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['usuario']) && isset($_GET['id'])){
     $entrada_id = $_GET['id'];
     $usuario_id = $_SESSION['usuario']['id'];
     
-    $sql = "DELETE FROM usuarios WHERE usuario_id = $usuario_id AND id = $entrada_id";
+    $sql = "DELETE FROM entradas WHERE usuario_id = $usuario_id AND id = $entrada_id";
     mysqli_query($db, $sql);
 }
 
