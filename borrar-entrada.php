b15a393f0b1c94547bac964ee917bf9e844f3a29
abel enrique <abel@abel.com>
2020-01-09 22:11:52 -0600

    se agrego metodo GET en borrar-entrada

--- borrar-entrada.php
+++ borrar-entrada.php
@@ -3,10 +3,10 @@
 
 //hacemos un if para verificar si existe el usuario identificado
 if(isset($_SESSION['usuario']) && isset($_GET['id'])){
-    $entrada_id = $GET['id'];
+    $entrada_id = $_GET['id'];
     $usuario_id = $_SESSION['usuario']['id'];
     
-    $sql = "DELETE FROM usuarios WHERE usuario_id = $usuario_id AND id = $entrada_id ";
+    $sql = "DELETE FROM usuarios WHERE usuario_id = $usuario_id AND id = $entrada_id";
     mysqli_query($db, $sql);
 }
 
