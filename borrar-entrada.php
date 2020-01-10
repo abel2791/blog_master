a90d2a4bcb78cb93cb0734ef587156497d05e0e9
abel enrique <abel@abel.com>
2020-01-09 21:55:11 -0600

    se añadio borrar entrada

---
+++ borrar-entrada.php
@@ -0,0 +1,13 @@
+<?php
+ require_once 'includes/conexion.php';
+
+//hacemos un if para verificar si existe el usuario identificado
+if(isset($_SESSION['usuario']) && isset($_GET['id'])){
+    $entrada_id = $GET['id'];
+    $usuario_id = $_SESSION['usuario']['id'];
+    
+    $sql = "DELETE FROM usuarios WHERE usuario_id = $usuario_id AND id = $entrada_id ";
+    mysqli_query($db, $sql);
+}
+
+header("Location: index.php");
